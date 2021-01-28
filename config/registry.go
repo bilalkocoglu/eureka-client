4a98822d47655a3de8974f01166547159fd1083f
Bilal <kocogluubilal@gmail.com>
2021-01-28 21:35:23 +0300

    added cloud config environments

---
+++ config/registry.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"github.com/bilalkocoglu/eureka-client/discovery"
+	"github.com/bilalkocoglu/eureka-client/model"
+	"github.com/bilalkocoglu/eureka-client/store"
+)
+
+func setRegistryConfigForCloud(cloudConfig model.CloudConfig, cfg *store.RegistryConfig) {
+	for _, s := range cloudConfig.PropertySources {
+		url := s.Source["registry.url"]
+		if cfg.URL == "" && url != nil && url != "" {
+			cfg.URL = url.(string)
+		}
+
+		username := s.Source["registry.username"]
+		if cfg.Username == "" && username != nil && username != "" {
+			cfg.Username = username.(string)
+		}
+
+		password := s.Source["registry.password"]
+		if cfg.Password == "" && password != nil && password != "" {
+			cfg.Password = password.(string)
+		}
+
+		useSSL := s.Source["registry.use-ssl"]
+		if useSSL != nil && useSSL != "" {
+			cfg.UseSSL = useSSL.(bool)
+		}
+
+	}
+}
+
+func ServiceRegister(cfg store.RegistryConfig) {
+	config := discovery.RegistrationVariables{ServiceRegistryURL: cfg.URL, UserName: cfg.Username, Password: cfg.Password}
+	go discovery.ManageDiscovery(config)
+}
