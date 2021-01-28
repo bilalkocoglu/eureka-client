dd20f2fd09b0406f674e1b68b37584cf3139c0ed
Bilal <kocogluubilal@gmail.com>
2021-01-28 22:40:22 +0300

    fix fill register config

--- main.go
+++ main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func ServiceRegister(cfg store.RegistryConfig) {
-	config := discovery.RegistrationVariables{ServiceRegistryURL: cfg.URL, UserName: cfg.Username, Password: cfg.Password}
+	config := discovery.RegistrationVariables{ServiceRegistryURL: cfg.URL, UserName: cfg.Username, Password: cfg.Password, UseSSL: cfg.UseSSL, AppName: cfg.AppName, AppPort: cfg.AppPort}
 	go discovery.ManageDiscovery(config)
 }
